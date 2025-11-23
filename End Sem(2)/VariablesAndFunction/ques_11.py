53354bc2d5eb6023085a1e355460c664144c7bc8
CRASHs4aint <samratkumarsahu228@gmail.com>
2025-11-23 17:44:27 +0530

    updated

---
+++ End Sem(2)/VariablesAndFunction/ques_11.py
@@ -0,0 +1,5 @@
+import datetime
+
+now=datetime.datetime.now()
+
+print(now.strftime("-%a %d %H-%M-%S IST %Y"))
---
+++ End Sem(2)/VariablesAndFunction/question_12.py
@@ -0,0 +1,4 @@
+temp=int(input("Enter temperature in celsius:"))
+
+fahernheit=temp*(9/5)+32
+print(fahernheit)
